696ea6b24a7a1e73fb205286483e86d997ddb6c5
Srlimao <w.back93@gmail.com>
2020-02-27 21:08:12 -0300

    1

---
+++ TodoAPI/Models/TodoItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TodoAPI
+{
+    public class TodoItem
+    {
+        public string Title { get; set; }
+
+        public DateTime DueDate { get; set; }
+
+        public bool IsChecked { get; set; }
+
+    }
+}
---
+++ TodoAPI/TodosManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoAPI
+{
+    public static class TodosManager
+    {
+        static List<TodoItem> todos = new List<TodoItem>();
+
+        public static void AddItem(string title)
+        {
+            TodoItem todo = new TodoItem();
+            todo.Title = title;
+            todo.IsChecked = false;
+            todo.DueDate = DateTime.Now;//todo
+
+        }
+
+        public static void RemoveItem(string title)
+        {
+
+        }
+
+        public static void SetCheckItem(bool check)
+        {
+
+        }
+
+        public static List<TodoItem> ListItems()
+        {
+            return todos;
+        }
+    }
+}
